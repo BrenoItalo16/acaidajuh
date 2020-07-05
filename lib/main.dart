cece9a93d7c1e5a35328ec88dd4111f335c8ed78
Breno Italo <srbrenoitalo16@gmail.com>
2020-07-05 17:30:40 -0300

    Iniciado Carrinho de compras - Aula 43

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:acaidajuh/models/product.dart';
 import 'package:acaidajuh/models/product_manager.dart';
 import 'package:acaidajuh/models/user_manager.dart';
+import 'package:acaidajuh/screens/cart/cart_screen.dart';
 import 'package:acaidajuh/screens/login/login_screen.dart';
 import 'package:acaidajuh/screens/product/product_screen.dart';
 import 'package:acaidajuh/screens/signup/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:acaidajuh/screens/base/base_screen.dart';
 import 'package:provider/provider.dart';
+
+import 'models/cart_manager.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +29,7 @@ class MyApp extends StatelessWidget {
           lazy: false,
         ),
         Provider(
-          create: (_) => UserManager(),
+          create: (_) => CartManager(),
           lazy: false,
         ),
       ],
@@ -56,6 +59,10 @@ class MyApp extends StatelessWidget {
                 builder: (_) => ProductScreen(
                   settings.arguments as Product,
                 ),
+              );
+            case '/cart':
+              return MaterialPageRoute(
+                builder: (_) => CartScreen(),
               );
             default:
               return MaterialPageRoute(builder: (_) => BaseScreen());
--- lib/models/cart_product.dart
+++ lib/models/cart_product.dart
@@ -1,4 +1,5 @@
 import 'package:acaidajuh/models/product.dart';
+import 'item_size.dart';
 
 class CartProduct {
   CartProduct.fromProduct(this.product) {
@@ -12,4 +13,14 @@ class CartProduct {
   String size;
 
   Product product;
+
+  ItemSize get itemSize {
+    if (product == null) return null;
+    return product.findSize(size);
+  }
+
+  num get unitPrice {
+    if (product == null) return 0;
+    return itemSize?.price ?? 0;
+  }
 }
