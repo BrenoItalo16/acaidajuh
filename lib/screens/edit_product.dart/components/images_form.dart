8e881c73054f84d8358e1f56166713830123326c
Breno Italo <srbrenoitalo16@gmail.com>
2020-07-10 23:08:55 -0300

    Editando tela de produto

---
+++ lib/screens/edit_product.dart/components/images_form.dart
@@ -0,0 +1,14 @@
+import 'package:acaidajuh/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ImagesForm extends StatelessWidget {
+  const ImagesForm(this.product);
+  final Product product;
+  @override
+  Widget build(BuildContext context) {
+    return FormField<List<dynamic>>(
+      initialValue: product.images,
+      builder: (state) {},
+    );
+  }
+}
--- lib/screens/edit_product.dart/edit_product_screen.dart
+++ lib/screens/edit_product.dart/edit_product_screen.dart
@@ -1,11 +1,20 @@
+import 'package:acaidajuh/models/product.dart';
+import 'package:acaidajuh/screens/edit_product.dart/components/images_form.dart';
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatelessWidget {
+  const EditProductScreen(this.product);
+  final Product product;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editar anúncio'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ImagesForm(product),
+        ],
       ),
     );
   }
