30879c9ef2b4042ef5ff9f4bbe2df81cfcb85c09
Breno Italo <srbrenoitalo16@gmail.com>
2020-07-11 20:59:42 -0300

    Criado o botão para excluir imagem - 65

--- lib/screens/edit_product.dart/components/images_form.dart
+++ lib/screens/edit_product.dart/components/images_form.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:acaidajuh/models/product.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
 class ImagesForm extends StatelessWidget {
@@ -8,7 +11,48 @@ class ImagesForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return FormField<List<dynamic>>(
       initialValue: product.images,
-      builder: (state) {},
+      builder: (state) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Carousel(
+            images: state.value.map(
+              (image) {
+                return Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    if (image is String)
+                      Image.network(
+                        image,
+                        fit: BoxFit.cover,
+                      )
+                    else
+                      Image.file(
+                        image as File,
+                        fit: BoxFit.cover,
+                      ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: Icon(Icons.remove),
+                        color: Colors.red,
+                        onPressed: () {
+                          state.value.remove(image);
+                          state.didChange(state.value);
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ).toList(),
+            dotSize: 4,
+            dotSpacing: 15,
+            dotBgColor: Colors.transparent,
+            dotColor: Theme.of(context).primaryColor,
+            autoplay: false, //* muda de imagem sozinho caso true
+          ),
+        );
+      },
     );
   }
 }
