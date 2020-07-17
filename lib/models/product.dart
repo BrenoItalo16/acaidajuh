7aa37ae1266ad44b66cb9f000d8c9100243ac53e
Breno Italo <srbrenoitalo16@gmail.com>
2020-07-17 16:46:14 -0300

    Salvando produto editado - 82

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
 import 'package:acaidajuh/models/item_size.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:uuid/uuid.dart';
 
 class Product extends ChangeNotifier {
   Product({this.id, this.name, this.description, this.images, this.sizes}) {
@@ -19,7 +22,9 @@ class Product extends ChangeNotifier {
   }
 
   final Firestore firestore = Firestore.instance;
+  final FirebaseStorage storage = FirebaseStorage.instance;
   DocumentReference get firestoreRef => firestore.document('products/$id');
+  StorageReference get storageRef => storage.ref().child('products').child(id);
 
   String id;
   String name;
@@ -28,6 +33,13 @@ class Product extends ChangeNotifier {
   List<ItemSize> sizes;
 
   List<dynamic> newImages;
+
+  bool _loading = false;
+  bool get loading => _loading;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
 
   ItemSize _selectedSize;
 
@@ -71,6 +83,7 @@ class Product extends ChangeNotifier {
   }
 
   Future<void> save() async {
+    loading = true;
     final Map<String, dynamic> data = {
       'name': name,
       'description': description,
@@ -83,6 +96,37 @@ class Product extends ChangeNotifier {
     } else {
       await firestoreRef.updateData(data);
     }
+
+    //images [url1, url2, url3]
+
+    final List<String> updateImages = [];
+
+    for (final newImage in newImages) {
+      if (images.contains(newImage)) {
+        updateImages.add(newImage as String);
+      } else {
+        final StorageUploadTask task =
+            storageRef.child(Uuid().v1()).putFile(newImage as File);
+        final StorageTaskSnapshot snapshot = await task.onComplete;
+        final String url = await snapshot.ref.getDownloadURL() as String;
+        updateImages.add(url);
+      }
+    }
+
+    for (final image in images) {
+      if (!newImages.contains(image)) {
+        try {
+          final ref = await storage.getReferenceFromUrl(image);
+          await ref.delete();
+        } catch (e) {
+          debugPrint('Falha ao deletar $image');
+        }
+      }
+    }
+
+    await firestoreRef.updateData({'images': updateImages});
+    images = updateImages;
+    loading = false;
   }
 
   Product clone() {
--- lib/screens/edit_product.dart/edit_product_screen.dart
+++ lib/screens/edit_product.dart/edit_product_screen.dart
@@ -1,7 +1,9 @@
 import 'package:acaidajuh/models/product.dart';
+import 'package:acaidajuh/models/product_manager.dart';
 import 'package:acaidajuh/screens/edit_product.dart/components/images_form.dart';
 import 'package:acaidajuh/screens/edit_product.dart/components/sizes_form.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class EditProductScreen extends StatelessWidget {
   EditProductScreen(Product p)
@@ -13,103 +15,122 @@ class EditProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(editing ? 'Editar produto' : 'Criar produto'),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: Form(
-        key: formKey,
-        child: ListView(
-          children: <Widget>[
-            ImagesForm(product),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  TextFormField(
-                    initialValue: product.name,
-                    decoration: const InputDecoration(
-                      hintText: 'Título',
-                      border: InputBorder.none,
-                    ),
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    validator: (name) {
-                      if (name.length < 6) return 'Título muito curto!';
-                      return null;
-                    },
-                    onSaved: (name) => product.name = name,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 4),
-                    child: Text(
-                      'A partir de',
+    return ChangeNotifierProvider.value(
+      value: product,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(editing ? 'Editar produto' : 'Criar produto'),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: Form(
+          key: formKey,
+          child: ListView(
+            children: <Widget>[
+              ImagesForm(product),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    TextFormField(
+                      initialValue: product.name,
+                      decoration: const InputDecoration(
+                        hintText: 'Título',
+                        border: InputBorder.none,
+                      ),
                       style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 13,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
                       ),
-                    ),
-                  ),
-                  Text(
-                    'R\$ ...',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: primaryColor,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text(
-                      'Descrição',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                  TextFormField(
-                    initialValue: product.description,
-                    style: const TextStyle(fontSize: 16),
-                    decoration: const InputDecoration(
-                        hintText: 'Descrição', border: InputBorder.none),
-                    maxLines: null,
-                    validator: (desc) {
-                      if (desc.length < 10) return 'Descrição muito curta';
-                      return null;
-                    },
-                    onSaved: (desc) => product.description = desc,
-                  ),
-                  SizesForm(product),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    height: 44,
-                    child: RaisedButton(
-                      onPressed: () {
-                        if (formKey.currentState.validate()) {
-                          formKey.currentState.save();
-
-                          product.save();
-                        }
+                      validator: (name) {
+                        if (name.length < 6) return 'Título muito curto!';
+                        return null;
                       },
-                      textColor: Colors.white,
-                      color: primaryColor,
-                      disabledColor: primaryColor.withAlpha(100),
-                      child: const Text(
-                        'Salvar',
-                        style: TextStyle(fontSize: 18.0),
+                      onSaved: (name) => product.name = name,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Text(
+                        'A partir de',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 13,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    Text(
+                      'R\$ ...',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: primaryColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text(
+                        'Descrição',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue: product.description,
+                      style: const TextStyle(fontSize: 16),
+                      decoration: const InputDecoration(
+                          hintText: 'Descrição', border: InputBorder.none),
+                      maxLines: null,
+                      validator: (desc) {
+                        if (desc.length < 10) return 'Descrição muito curta';
+                        return null;
+                      },
+                      onSaved: (desc) => product.description = desc,
+                    ),
+                    SizesForm(product),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Consumer<Product>(
+                      builder: (_, product, __) {
+                        return SizedBox(
+                          height: 44,
+                          child: RaisedButton(
+                            onPressed: !product.loading
+                                ? () async {
+                                    if (formKey.currentState.validate()) {
+                                      formKey.currentState.save();
+
+                                      await product.save();
+                                      context
+                                          .read<ProductManager>()
+                                          .update(product);
+                                      Navigator.of(context).pop();
+                                    }
+                                  }
+                                : null,
+                            textColor: Colors.white,
+                            color: primaryColor,
+                            disabledColor: primaryColor.withAlpha(100),
+                            child: product.loading
+                                ? CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation(
+                                      Colors.white,
+                                    ),
+                                  )
+                                : const Text(
+                                    'Salvar',
+                                    style: TextStyle(fontSize: 18.0),
+                                  ),
+                          ),
+                        );
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
